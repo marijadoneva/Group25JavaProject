4ff156accdbd24cf867fff647a3f5e6c14b45a9f
Hanan_Cydeo <Hanan_Cydeo>
2023-05-25 16:31:13 -0500

    odd or even task

---
+++ src/hanan/OddOrEven.java
@@ -0,0 +1,12 @@
+package hanan;
+
+public class OddOrEven {
+        public static String identify(int number) {
+            if (number % 2 == 0) {
+                return "Even";
+            } else {
+                return "Odd";
+            }
+        }
+    }
+

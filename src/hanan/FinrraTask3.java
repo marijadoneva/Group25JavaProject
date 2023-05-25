5ac7659fb6b0c6aebea3f70ff1b601ebd090532f
Hanan_Cydeo <Hanan_Cydeo>
2023-05-25 16:39:21 -0500

    finrra task

---
+++ src/hanan/FinrraTask3.java
@@ -0,0 +1,24 @@
+package hanan;
+
+public class FinrraTask3 {
+    public static void main(String[] args) {
+
+
+        int number = 15;
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FINRA");
+
+        } else if (number % 3 == 0) {
+            System.out.println("FIN");
+
+        } else if (number % 5 == 0) {
+            System.out.println("RA");
+
+
+        } else {
+            System.out.println(number);
+        }
+
+
+    }}

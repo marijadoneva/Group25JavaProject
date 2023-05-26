8c29a3c86f1c9808c37bd79e28159e20621d73cc
TuyanaChimitova <chimitova1993@gmail.com>
2023-05-25 23:07:53 -0500

    Class EvenOdd created from Tuyana Chimitova

--- src/tuyana/Week01_Q01.java
+++ src/tuyana/Week01_Q01.java
@@ -5,5 +5,11 @@ public class Week01_Q01 {
     public static void main(String[] args) {
         System.out.println("Hello from Tuyana!");
         System.out.println("How are you?");
+
+        System.out.println("-----------------------");
+
+
+
+
     }
 }

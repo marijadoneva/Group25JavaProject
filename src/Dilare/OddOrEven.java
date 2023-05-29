07ebcedecf68a22886d174a614a53f674395a651
Dilar22 <124005003+Dilar22@users.noreply.github.com>
2023-05-29 12:33:10 -0500

    OddOrEven.java

--- src/Dilare/OddOrEven.java
+++ src/Dilare/OddOrEven.java
@@ -10,3 +10,12 @@ public class OddOrEven {
         }
     }
 }
+
+/*
+
+ 1 - Numbers- Odd or even:
+Write  a method which can identify given number is even or odd
+Output ex:
+identify(5); ->"Odd"
+identify(6); ->"Even"
+*/

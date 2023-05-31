bb4610a4f5eb8a0cd145cd12dc0703e822eb421e
Marija <marijadoneva9@gmail.com>
2023-05-31 15:40:30 -0400

    Changes in SwapNumbers file

--- src/Marija/week2/SwapNumbers.java
+++ src/Marija/week2/SwapNumbers.java
@@ -1,4 +1,18 @@
 package Marija.week2;
 
 public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+
+
+
+
+
+    }
 }
+/*
+1.Numbers -- Swap Numbers
+ Swap two variable values without using a third variable
+ */

ee8e95a2c8b5d5c58a4750b4851c55917fc4e4d6
Marija <marijadoneva9@gmail.com>
2023-05-31 15:39:04 -0400

    Create divisible SwapNumbers file

--- src/Marija/week2/DivisibleNumbers.java
+++ src/Marija/week2/DivisibleNumbers.java
@@ -3,7 +3,28 @@ package Marija.week2;
 public class DivisibleNumbers {
     public static void main(String[] args) {
 
+        for (int i = 1; i <=100 ; i++) {
 
+            if (i%3==0 || i%5==0 || i%15==0){
+
+                System.out.println(i);
+
+            }
+
+
+//            String divisibleBy15 = "";
+//            String divisibleBy5 = "";
+//            String DivisibleBy3 = "";
+//
+//            if (i%3==0 || i%5==0 || i%15==0){
+//                divisibleBy15=i + " ";
+//            } else if (i%5==0) {
+//                divisibleBy5= i + " ";
+//            }
+
+        }
+
+        System.out.println();
 
 
     }

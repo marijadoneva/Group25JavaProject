d42e1ce08571bf76d43ae0cf072749ab44a8513b
Marija <marijadoneva9@gmail.com>
2023-06-03 21:50:25 -0400

    Solution to Prime Numbers task and changes in Reverse negative numbers task

--- src/Marija/week3/ReverseNegativeNumber.java
+++ src/Marija/week3/ReverseNegativeNumber.java
@@ -1,7 +1,22 @@
 package Marija.week3;
 
 public class ReverseNegativeNumber {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public int ReverseNegative(int a){
+        int b=0;
+
+
+        return b;
+    }
+
 }
+
+
 /*
 Write a return method that can reverse digits of a negative number and return it as int
 input: -35  output: -53

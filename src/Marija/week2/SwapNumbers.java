bb3cf137844338dbb9d49c7fb0eeb91bab7b8471
Marija <marijadoneva9@gmail.com>
2023-06-03 20:54:38 -0400

    Solution of SwapNumbers

--- src/Marija/week2/SwapNumbers.java
+++ src/Marija/week2/SwapNumbers.java
@@ -6,7 +6,12 @@ public class SwapNumbers {
         int a = 2;
         int b = 3;
 
+        a=a*b;
+        b=a/b;
+        a=a/b;
 
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
 
 

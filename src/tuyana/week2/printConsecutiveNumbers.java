7e92cbf54dea7cae4c4dc1da603734d9ee8aced9
TuyanaChimitova <chimitova1993@gmail.com>
2023-06-05 14:41:03 -0500

    file printConsecutiveNumbers updated

--- src/tuyana/week2/printConsecutiveNumbers.java
+++ src/tuyana/week2/printConsecutiveNumbers.java
@@ -1,10 +1,45 @@
 package tuyana.week2;
 
+import java.util.Scanner;
+
 public class printConsecutiveNumbers {
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
 
 
-}
+        for (int i = 1; i < N; i++) {
+
+            if (i%2==0 && i%3==0 && i%5==0){
+                System.out.println("CodilityTestCoders");
+            }else if (i%2==0 && i%3==0) {
+                System.out.println("CodilityTest");
+            } else if (i%2==0 && i%5==0 ) {
+                System.out.println("CodilityCoders");
+            } else if (i%3==0 && i%5==0) {
+                System.out.println("TestCoders");
+            } else if (i%5==0) {
+                System.out.println("Coders");
+            } else if (i%3==0) {
+                System.out.println("Test");
+            } else if (i%2==0) {
+                System.out.println("Codility");
+            }else {
+                System.out.println(i);
+            }
+
+
+        }
+
+
+        input.close();
+    }
+    }
+
+
+
 
 
 /*

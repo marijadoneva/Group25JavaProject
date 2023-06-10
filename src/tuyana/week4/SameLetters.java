497c418b7b2bbc27fbd61886d8b65284cec0713f
TuyanaChimitova <chimitova1993@gmail.com>
2023-06-09 22:26:35 -0500

    class SameLetters created

---
+++ src/tuyana/week4/SameLetters.java
@@ -0,0 +1,36 @@
+package tuyana.week4;
+
+import java.util.Arrays;
+
+public class SameLetters {
+
+    public static boolean madeOutOfSameLetters(String a, String b){
+        if(a == null){
+            return b == null;
+        }else if(b == null){
+            return false;
+        }
+        char[] left = a.toCharArray();
+        char[] right = b.toCharArray();
+        Arrays.sort(left);
+        Arrays.sort(right);
+        return Arrays.equals(left, right);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(madeOutOfSameLetters("abc", "cab"));
+        System.out.println(madeOutOfSameLetters("abc", "abb"));
+    }
+
+
+
+
+}
+
+/*
+String - Same letters
+  Write a return method that checks if a string is built out of the same letters as another string.
+    Ex: same("abc", "cab"); ==> true
+        same("abc", "abb"); ==> false
+ */

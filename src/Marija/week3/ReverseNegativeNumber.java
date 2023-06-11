42aae3ecbae10c57584dcba122e6ab25adde8a25
Marija <marijadoneva9@gmail.com>
2023-06-10 23:37:22 -0400

    Solved ReverseNegativeNUmber

--- src/Marija/week3/ReverseNegativeNumber.java
+++ src/Marija/week3/ReverseNegativeNumber.java
@@ -1,16 +1,40 @@
 package Marija.week3;
 
+import java.util.Scanner;
+
 public class ReverseNegativeNumber {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a negative number");
+        int number = input.nextInt();
+        ReverseNegative(number);
+
+        System.out.println("Please enter a negative number");
+        number = input.nextInt();
+        ReverseNegative(number);
 
 
+        input.close();
     }
 
-    public int ReverseNegative(int a){
+    public static int ReverseNegative(int a){
         int b=0;
-        //working on this task
 
+        a*=-1;//35
+        String abc="";
+        String num = ""+a;
+
+        for (int i = num.length()-1; i >=0; i--) {
+
+            char each = num.charAt(i);
+            abc+=each;
+        }
+
+        b=Integer.valueOf(abc);
+        b*=-1;
+
+        System.out.println(b);
         return b;
     }
 

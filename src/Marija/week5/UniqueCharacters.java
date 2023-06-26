914bac4696e560c670fd8df2e8a0ba4fa36a11b0
Marija <marijadoneva9@gmail.com>
2023-06-26 00:36:03 -0400

    Working on UniqueCharacters Task

--- src/Marija/week5/UniqueCharacters.java
+++ src/Marija/week5/UniqueCharacters.java
@@ -6,6 +6,19 @@ public class UniqueCharacters {
     }
 
 
+    public static String unique(String word){
+        String uniqueChar="";
+
+        for (int i = 0; i<=word.length()-1 ; i++) {
+            char uniq = word.charAt(i);
+           // if ()
+
+        }
+
+        System.out.println(uniqueChar);
+
+        return uniqueChar;
+    }
 
 
 

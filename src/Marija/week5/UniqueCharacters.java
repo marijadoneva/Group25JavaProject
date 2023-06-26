034c48e44b4970b1aaa6499c3d3269e6bb335846
Marija <marijadoneva9@gmail.com>
2023-06-26 00:31:15 -0400

    Solve UniqueCharacters Task

---
+++ src/Marija/week5/UniqueCharacters.java
@@ -0,0 +1,18 @@
+package Marija.week5;
+
+public class UniqueCharacters {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+}
+
+
+/*
+1. Write a return  method that can find the unique characters from the String
+Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+ */

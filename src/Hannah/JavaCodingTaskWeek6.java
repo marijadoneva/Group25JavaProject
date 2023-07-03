025562002c28b7ef276363dd6711f5ee6aa786ef
Hqputu <Hqputu@yahoo.com>
2023-07-03 15:36:48 -0500

    Week6 Java coding task solution created

---
+++ src/Hannah/JavaCodingTaskWeek6.java
@@ -0,0 +1,50 @@
+package Hannah;
+
+import static Hannah.JavaCodingTaskWeek6.DigitSum.sumOfDigits;
+
+public class JavaCodingTaskWeek6 {
+    public static class DigitSum {
+        public static int sumOfDigits(String string){
+            int digitSum = 0;
+            for (char c : string.toCharArray()){
+                if(Character.isDigit(c)){
+                    digitSum += Character.getNumericValue(c);
+                }
+            }
+            return digitSum;
+        }
+    }
+        public static void main(String[]arg){
+        String string = "12 java 5 apple 3";
+        int result = sumOfDigits(string);
+            System.out.println(result);
+        }
+
+
+
+
+
+       /*
+        **Java Coding Tasks - Week6  **
+
+  **String_SumOfAllDigits**
+```
+Write a method that can return the sum of the digits in a string
+Ex:  "12 java 5 apple 3"  ==>  20
+```
+
+
+        }
+
+        public static void main(String[] args) {
+            String string = "12 java 5 apple 3";
+            int result = sumOfDigits(string);
+            System.out.println(result);  // Output: 20
+        }
+    }
+
+
+ */
+
+
+}
